b6efad0928748545e659f3876457b37a61c0d4dc
Ashraf Rasulov <ashee87.news@gmail.com>
2026-06-19 21:11:05 +0500

    Исправил файл зависимостей

--- requirements.bat
+++ requirements.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Сканирование проекта и создание requirements.txt...
 pip install pipreqs
-:: Команда --force перезапишет файл, если он уже есть
-pipreqs . --force
-echo Файл requirements.txt успешно создан на основе твоего кода!
+:: Команда --force перезапишет файл
+pipreqs . --force --encoding=utf-8
+echo Файл requirements.txt успешно создан!
 pause
